3cad75c9c7b33445532eeb2e6aaebc1bcf6e2f18
OwO-svg <55345245+OwO-svg@users.noreply.github.com>
2019-10-06 18:26:16 +0200

    added main files

---
+++ app.js
@@ -0,0 +1,42 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+const greetings = ['bonjour', 'comment allez vous aujord\'hui?', 'flaime de répondre']
+const weather = ['je sais pas moi!', 'aucune idée', "regared dehors!"]
+
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function () {
+	console.log('voice is activated, you can speak to micro');
+};
+
+recognition.onresult = function(event) {
+	const current = event.resultIndex;
+	const transcript = event.results[current][0].transcript;
+	content.textContent = transcript;
+	readOutLoud(transcript);
+};
+
+btn.addEventListener('click', () => {
+	recognition.start();
+});
+
+function readOutLoud(message){
+	const speech = new SpeechSynthesisUtterance();
+
+	if(message.includes('Bonjour')){
+		const ft = greetings[Math.floor(Math.random() * greetings.length)];
+		speech.text = ft;
+
+	}
+
+	
+	speech.volume = 1;
+	speech.rate = 1;
+	
+
+	window.speechSynthesis.speak(speech);
+}
+
